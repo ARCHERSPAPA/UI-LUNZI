0777d8741d46514ce0e70263510d07765da484a6
xjy <763697867@qq.com>
2019-06-04 22:03:02 +0800

    使用了chai-spies 进行了事件的伪装测试事件正常运行

--- src/app.js
+++ src/app.js
@@ -13,4 +13,64 @@ new Vue({
         isload2:false
         }
 
+
 })
+//单元测试
+import modetest from'chai'
+import  spy from 'chai-spies'
+modetest.use( spy)
+const  expect = modetest.expect
+{
+    const constructor = Vue.extend(ButtonS)
+    const button = new constructor(
+        {
+            propsData: {
+                icon: 'you'
+            }
+        })
+    button.$mount('#test')
+    let useelment = button.$el.querySelector('use')
+    expect(useelment.getAttribute('xlink:href')).to.eq("#icon-you")
+
+    button.$el.remove()
+    button.$destroy()
+}
+//事件测试
+{
+    const constructor = Vue.extend(ButtonS)
+    const gbutton = new constructor(
+        {
+            propsData: {
+                icon: 'you'
+            }
+        })
+    gbutton.$mount()
+    let spy= modetest.spy(function(){
+
+    })
+    gbutton.$on('click',spy)
+    let useelment = gbutton.$el
+   useelment.click()
+    expect(spy).to.have.been.called()
+    console.log(useelment)
+    gbutton.$el.remove()
+    gbutton.$destroy()
+}
+{
+ const div = document.createElement('div')
+    document.body.appendChild(div)
+    const constructor=Vue.extend(ButtonS)
+    const vm=new constructor({
+        propsData:{
+            icon:'you',
+            icondrection:'right'
+        }
+    })
+    vm.$mount(div)
+    let svg= vm.$el.querySelector('svg')
+    let{order}=window.getComputedStyle(svg)
+    console.log(order)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
